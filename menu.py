4288752a8c9931dd529ab42556053b2b837185d8
Isah Jacob <Isahjacob0@gmail.com>
2022-10-29 01:00:46 +0100

    created the menu file that model each menu item

---
+++ menu.py
@@ -0,0 +1,12 @@
+class MenuItem:
+    """Models each menu items"""
+    def __init__(self, name, water, milk, coffee, cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee
+        }
+
+        
